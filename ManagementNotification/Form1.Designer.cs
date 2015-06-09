e3ce41a8b2128e3eabecf2f4fdd9b484c5f7a570
Mercury201 <sakuya_maid@yahoo.co.jp>
2015-06-09 16:30:23 +0900

    Commit2

--- ManagementNotification/Form1.Designer.cs
+++ ManagementNotification/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(118, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HelloWorld";
+            this.label1.Text = "HelloWorld2";
             // 
             // Form1
             // 
