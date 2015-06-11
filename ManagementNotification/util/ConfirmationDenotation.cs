0b3d9885b2479d4c0f4dc482992632b088cac097
Takuma <kbc12a13@stu.kawahara.ac.jp>
2015-06-11 14:08:18 +0900

    Confirmationクラス(確認内容を表示するクラス)の追加

---
+++ ManagementNotification/util/ConfirmationDenotation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementNotification.util
+{
+    class ConfirmationDenotation
+    {
+        public void a()
+        {
+
+        }
+    }
+}
