c5df95de06a76386c15b88a72c4ccb083850ac38
Mercury201 <sakuya_maid@yahoo.co.jp>
2015-06-12 18:37:08 +0900

    画面遷移ver1.0

--- ManagementNotification/Confirmation.cs
+++ ManagementNotification/Confirmation.cs
@@ -12,6 +12,8 @@ namespace ManagementNotification.util
 {
     public partial class Confirmation : Form
     {
+        Form pre;
+
         ConfirmationDenotation CD = new ConfirmationDenotation();
 
         public Confirmation()
@@ -19,10 +21,24 @@ namespace ManagementNotification.util
             InitializeComponent();
         }
 
+        public Confirmation(Form form)
+        {
+            InitializeComponent();
+            pre = form;
+        }
+
         private void Confirmation_Load(object sender, EventArgs e)
         {
             //CD.DateDenotation(treeView1);
-            CD.DateDenotation(View1);
+            //CD.DateDenotation(View1);
+        }
+
+        //closingはclose()が呼び出される
+        //×が押されてclose()の処理のあとに呼び出される
+        private void Confirmation_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Console.Write("確認画面から閉じます");
+            Application.Exit();
         }
     }
 }
