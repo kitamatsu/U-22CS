df4399fe355f0896336d22ca98973256f22bbcf6
Takuma <kbc12a13@stu.kawahara.ac.jp>
2015-06-26 15:33:25 +0900

    新規アカウント管理画面

--- ManagementNotification/AccountCreate.cs
+++ ManagementNotification/AccountCreate.cs
@@ -17,12 +17,34 @@ namespace ManagementNotification
         public AccountCreate()
         {
             InitializeComponent();
+            
         }
 
         public AccountCreate(Form form)
         {
             InitializeComponent();
             pre = form;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(userNameTB.Text.Length < 1 ||
+                emailTB.Text.Length < 1 ||
+                passTB.Text.Length < 1 ||
+                passConfirmationTB.Text.Length < 1)
+            {
+                label13.Text = "未入力項目があります";
+            }
+            else if (passTB.Text != passConfirmationTB.Text)
+            {
+                label13.Text = "パスワードが一致しません";
+            }
+            else
+            {
+                MessageBox.Show("入力完了");
+            }
+            
         }
     }
 }
