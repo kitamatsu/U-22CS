d0626aca54439bccfabdde40ebd5f7a30c75821b
Takuma <kbc12a13@stu.kawahara.ac.jp>
2015-07-30 13:16:27 +0900

    同意チェックボックス修正

--- ManagementNotification/AccountCertification.cs
+++ ManagementNotification/AccountCertification.cs
@@ -112,7 +112,15 @@ namespace ManagementNotification
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            this.button1.Enabled = true;
+            if (checkBox1.Checked)
+            {
+                this.button1.Enabled = true;
+            }
+            else
+            {
+                this.button1.Enabled = false;
+            }
+            
         }
     }
 }
